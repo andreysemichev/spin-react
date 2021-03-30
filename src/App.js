714528e0419a6659f1208fbd7e02f1cd8d3a3f0f
andreysemichev <andreysemichev@icloud.com>
2021-03-30 20:30:55 +0400

    Add absolute paths

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import image from "assets/img/t-shirt.jpg";
 import "./App.module.scss";
 
 const App = () => {
     return (<>
         <h1>Hello, World!</h1>
+        <img src={image} alt="img" />
     </>);
 };
 

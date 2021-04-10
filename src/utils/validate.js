505d660b37bd347d3613c090110ed75236893f68
andreysemichev <andreysemichev@icloud.com>
2021-04-11 02:44:41 +0400

    Remove formik and add validator

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,18 +1,15 @@
+import isEmail  from "validator/lib/isEmail";
+import isLength  from "validator/lib/isLength";
+
 export class Validate {
     static email = email => {
-        // eslint-disable-next-line
-        const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         
         const info = {
             isValid: true,
             msg: ""
         };
 
-        if (/[а-я]/i.test(email)) {
-            info.isValid = false;
-            info.msg = "Email is not valid";
-
-        } else if (!re.test(email)) {
+        if (!isEmail(email)) {
             info.isValid = false;
             info.msg = "Email is not valid";
         }
@@ -26,11 +23,7 @@ export class Validate {
             msg: ""
         };
 
-        if (password.length === 0) {
-            info.isValid = false;
-            info.msg = "Enter password";
-            
-        } else if (password.length < 3 || password.length > 32) {
+        if (!isLength(password, {min: 3, max: 32})) {
             info.isValid = false;
             info.msg = "Password must be between 3 and 12 characters";
         }
